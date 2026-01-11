352d96ebe96080ea41316eb780d1ba9196d1338f
Steve Biedermann <hardliner66@gmail.com>
2026-01-11 12:30:01 +0000

    change back db path

--- src/config.rs
+++ src/config.rs
@@ -12,20 +12,11 @@ pub struct Config {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            db_path: PathBuf::from(DEFAULT_DB_NAME),
+            db_path: directories::BaseDirs::new()
+                .map(|bd| bd.data_local_dir().to_path_buf())
+                .unwrap_or_else(|| "~/.local/share".into())
+                .join("issuecraft")
+                .join(DEFAULT_DB_NAME),
         }
     }
 }
-
-// TODO: reenable later. for testing a local file is okay.
-// impl Default for Config {
-//     fn default() -> Self {
-//         Self {
-//             db_path: directories::BaseDirs::new()
-//                 .map(|bd| bd.data_local_dir().to_path_buf())
-//                 .unwrap_or_else(|| "~/.local/share".into())
-//                 .join("issuecraft")
-//                 .join(DEFAULT_DB_NAME),
-//         }
-//     }
-// }
